8432fa08fd3a648bf0292de287cc0f80850270f0
Ian Ames <iames@riotgames.com>
2015-01-15 21:41:40 -0800

    working on importing client side js

---
+++ src/javascript/main.js
@@ -0,0 +1,7 @@
+$(function(){
+  $('#editButton').on('click', '#editButton', function(){
+      //var parameters = { search: $(this).val() };
+      //$.get( '/searching',parameters, function(data) {
+      //$('#results').html(data);
+    }).alert('Ohai');
+  });
