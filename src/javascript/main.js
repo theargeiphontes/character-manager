5a97202d62f6df0aaca56550157f5075dbdf7642
Ian Ames <iames@riotgames.com>
2015-04-02 18:50:51 -0700

    edit working!

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,10 +1,5 @@
 $(document).ready(function() {
-  $('body').on('click', '.editChar', function() {
-    console.log('edit onclick');
-    $.get('/pathfinder/edit', this.character, function () {
-      console.log('edit get');
-      console.log('char: ' + this.character);
-      
-    });
+  $('.editChar').on('click', function() {
+    $('.edit').prop('disabled', false);
   });
 });
