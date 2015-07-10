680b49219ca2c85f1cb0c65d6dad49b76feb12a8
Ian Ames <iames@riotgames.com>
2015-07-09 21:18:30 -0700

    starting pathfinder character logic, tests fail

---
+++ spec/unit/pathfinderCharacter.spec.js
@@ -0,0 +1,40 @@
+var expect = require('chai').expect;
+var Character = require('../../src/models/PathfinderCharacter.js');
+
+var __id = 0;
+var __charData = {
+  'name': 'Ian',
+  'game': 'pathfinder'
+  'stats': {
+    'str': '10',
+    'dex': '15',
+    'con': '10',
+    'int': '14',
+    'wis': '12',
+    'cha': '14'
+  },
+  'class': {
+    'rogue': '1'
+  }
+};
+
+describe('pathfinder character', function() {
+  var character;
+
+  beforeEach(function() {
+    character = new PathfinderCharacter(__id, __charData);
+  });
+
+  describe('#get()', function() {
+    it('fetch stats', function() {
+      var stats = character.getStats();
+      var str = character.getStat('str');
+
+      expect(stats).to.equal(__chardata['stats']);
+      expect(str).to.equal(__chardata['stats']['str']);
+    });
+
+    it('fetch class details', function() {
+    });
+  });
+});
