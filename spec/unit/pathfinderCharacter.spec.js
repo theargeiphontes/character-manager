58ddc0800753b708bb2dc8ec08420fe263efbf5a
Ian Ames <iames@riotgames.com>
2015-07-20 21:42:01 -0700

    updating PathfinderCharacter API + tests

--- spec/unit/pathfinderCharacter.spec.js
+++ spec/unit/pathfinderCharacter.spec.js
@@ -2,7 +2,7 @@ var expect = require('chai').expect;
 var PathfinderCharacter = require('../../src/models/PathfinderCharacter.js');
 
 var __id = 0;
-var __charData0 = {
+var __charData = {
   'name': 'Ian',
   'game': 'pathfinder',
   'stats': {
@@ -22,7 +22,7 @@ describe('pathfinder character', function() {
   var character;
 
   beforeEach(function() {
-    character = new PathfinderCharacter(__id, __charData0);
+    character = new PathfinderCharacter(__id, __charData);
   });
 
   describe('#get()', function() {
@@ -30,11 +30,99 @@ describe('pathfinder character', function() {
       var stats = character.getStats();
       var str = character.getStat('str');
 
-      expect(stats).to.equal(__chardata['stats']);
-      expect(str).to.equal(__chardata['stats']['str']);
+      expect(stats).to.equal(__charData['stats']);
+      expect(str).to.equal(__charData['stats']['str']);
     });
 
     it('fetch class details', function() {
+      var charClassList = character.getClassList();
+
+      for(var charClass in charClassList) {
+        expect(charClass).to.equal('rogue');
+        expect(charClassList[charClass]).to.equal('1');
+      }
+    });
+
+    it('fetch specific class level', function() {
+      expect(character.getClassLevel('rogue')).to.equal('1');
+    });
+
+    it('fail to get malformed stat name', function() {
+      expect(function() { character.getStat('charm') })
+        .to.throw(/Stat not found/);
+    });
+
+    it('fail to fetch undefined classList', function() {
+      var myCharData = {
+        'name': 'Ian',
+        'game': 'pathfinder',
+        'stats': {
+          'str': '10',
+          'dex': '15',
+          'con': '10',
+          'int': '14',
+          'wis': '12',
+          'cha': '14'
+        },
+        'fishy': {
+          'rat': '1'
+        }
+      }
+      var myChar = new PathfinderCharacter(1, myCharData);
+      expect(function() { myChar.getClassList() })
+        .to.throw(/No character classes found/);
+    });
+  });
+
+  describe('#set()', function() {
+    it('set name', function() {
+      character.set('name', 'Ian the Great');
+      expect(character.get('name')).to.equal('Ian the Great');
+    });
+
+    it('set stats', function() {
+      character.setStat('str', '12');
+      expect(character.getStat('str')).to.equal('12');
+    });
+
+    it('add a class level', function() {
+      character.updateClass('rogue', '2');
+      expect(character.getClassList()['rogue']).to.equal('2');
+    });
+
+    it('add new class / mutliclass', function() {
+      character.addClass('fighter', '2');
+      expect(character.getClassList()['fighter']).to.equal('2');
+    });
+
+    it('fail to add new class due to string', function() {
+      expect(function() { character.addClass('fighter', 'pie') })
+        .to.throw(/Level is not an int/);
+    }); 
+
+    it('fail to update class not in classList', function() {
+      expect(function() { character.updateClass('swordmage', '5') })
+        .to.throw(/not found in list of classes/);
+    });
+
+    it('fail to update class due to level cap', function() {
+      expect(function() { character.updateClass('rogue', '20') })
+        .to.throw(/exceeds level cap/);
+    });
+
+    it('fail to update class level by passing string', function() {
+      expect(function() { character.updateClass('rogue', 'pie') })
+        .to.throw(/Level is not an int/);
+    });
+
+    it('fail to update stats by passing string', function(){
+      expect(function() { character.setStat('str', 'pie'); })
+        .to.throw(/statValue is not an int/);
+    });
+
+    it('fail to update stats with out of bounds value', function(){
+      expect(function() { character.setStat('str', '500'); })
+        .to.throw(/statValue out of bounds/);
     });
   });
 });
--- src/models/PathfinderCharacter.js
+++ src/models/PathfinderCharacter.js
@@ -1,8 +1,101 @@
+var Character = require('./Character.js');
+
+var LEVEL_CAP = 20;
+
 function PathfinderCharacter (id, __char) {
     this.id = id;
-    this.name = __char.name;
-    this.stats = __char.stats;
-    this.json = __char;
+    this.JSON = __char;
 }
+PathfinderCharacter.prototype = new Character(this.id, this.JSON);
+
+// TODO: should I wrap my errior checking into functions or as is?
+// TODO: JSON saves as strings, should I parse all my ints to int at a class level then when 
+// I save everything, convert it back to strings?
+
+Character.prototype.getStats = function() {
+  if(this.JSON['stats'] === undefined) {
+    throw new Error('Stat not found');
+  }
+  return this.JSON['stats'];
+};
+
+Character.prototype.getStat = function(stat) {
+  if(this.JSON['stats'][stat] === undefined) {
+    throw new Error('Stat not found');
+  }
+  return this.JSON['stats'][stat];
+};
+
+Character.prototype.getClassList = function() {
+  if(this.JSON['class'] === undefined) {
+    throw new Error('No character classes found');
+  }
+  return this.JSON['class'];
+};
+
+// TODO: Do I need this?
+Character.prototype.getClassLevel = function(className) {
+  if(this.JSON['class'] === undefined || this.JSON['class'][className] === undefined ) {
+    throw new Error(className + ' not found');
+  }
+  return this.JSON['class'][className];
+};
+
+Character.prototype.setStat = function(stat, statValue) {
+  if(this.JSON['stats'][stat] === undefined) {
+    throw new Error('Stat key not found');
+  } 
+  if(isNaN(parseInt(statValue, 10))) {
+    throw new Error('statValue is not an int');
+  }
+  if(parseInt(statValue, 10) > 200) {
+    throw new Error('statValue out of bounds');
+  }
+  this.JSON['stats'][stat] = statValue;
+};
+
+// TODO: Break out checking level cap into its own private function
+// TODO: too much parse int?
+
+Character.prototype.addClass = function(className, level) {
+  this.level = parseInt(level);
+  if(this.JSON['class'] === undefined) {
+    throw new Error('No character classes found');
+  } 
+  if(isNaN(this.level, 10)) {
+    throw new Error('Level is not an int');
+  }
+
+  // Checking level cap
+  var charLevel = 0;
+  for(var charClass in this.JSON['class']) {
+    charLevel += parseInt(this.JSON['class'][charClass]);
+  }
+  if(charLevel + this.level > LEVEL_CAP) {
+    throw new Error('Adding' + className + ' exceeds level cap >> ' + LEVEL_CAP);
+  }
+  this.JSON['class'][className] = this.level.toString();
+};
+
+
+Character.prototype.updateClass = function(className, level) {
+  this.level = parseInt(level);
+  if(this.JSON['class'][className] === undefined) {
+    throw new Error(className + ' not found in list of classes');
+  } 
+  if(isNaN(this.level, 10)) {
+    throw new Error('Level is not an int');
+  }
+
+  // Checking level cap
+  var charLevel = 0;
+  for(var charClass in this.JSON['class']) {
+    charLevel += parseInt(this.JSON['class'][charClass]);
+  }
+  if(charLevel + this.level > LEVEL_CAP) {
+    throw new Error('Adding ' + className + ' exceeds level cap >> ' + LEVEL_CAP);
+  }
+  this.JSON['class'][className] = this.level.toString();
+};
 
 module.exports = PathfinderCharacter;
